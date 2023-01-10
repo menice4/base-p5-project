e3d571f30649e2bdaa2601d47fdf8c94290c3a8f
menice4 <95630289+menice4@users.noreply.github.com>
2023-01-10 11:18:44 +0000

    made sin dot

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,18 @@
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    let x = 0;
+    let y = 0;
+    let theta = 0; 
+    let amplitude = windowHeight/8;
+    let frequency = windowWidth/8;
+    let maxAngle = (windowWidth/frequency) * TWO_PI;
+    while (theta < maxAngle) {
+        y  = sin(theta) * amplitude; 
+        ellipse(x, y + height/2, 16);
+        theta += 0.2;
+        x = (theta/maxAngle) * windowWidth;
+    }
 }
 function draw() {
 }
