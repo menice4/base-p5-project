e1c54820c805cfe207be0cae77355a8787bbed35
menice4 <95630289+menice4@users.noreply.github.com>
2023-01-10 12:20:02 +0000

    Sqaures

--- js/sketch.js
+++ js/sketch.js
@@ -1,60 +1,54 @@
 
-    let x = 0;
-    let y = 0;
-    let theta = 0;
-    let inc = 0.05;
-    let offset = 0; // the amount offset from begening of wave
-    let amplitude; 
-    let frequency; 
-    let maxAngle; 
+let x = 0;
+let y = 0;
+let theta = 0;
+let inc = 0.05;
+let offset = 0; // the amount offset from begening of wave
+let amplitude;
+let frequency;
+let maxAngle;
 
 function setup() {
-     amplitude = windowHeight/2.5;
-     frequency = windowWidth/8;
-     maxAngle = (windowWidth/frequency) * TWO_PI;
+    amplitude = windowHeight / 2;
+    frequency = windowWidth / 5;
+    maxAngle = (windowWidth / frequency) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
-   
+
 }
 function draw() {
     background(0);
     noStroke();
-    while (theta < maxAngle + offset ) {
-       //sin
-       
-        fill(0, 255, 0)
-        y  = sin(theta) * amplitude; 
-        ellipse(x, y + height/2, 16);
-        theta += 0.2;
+    let length = 40;
+    while (theta < maxAngle + offset) {
+
+        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/2);
+        for (i = length; i > 0; i--) {
+            y = sin(theta - (i * 0.08)) * amplitude;
+            fill(0, 255 -(255/length * i), 0);
+            square(x, y + height / 2, 16);
+        }
+
+        for (i = length; i > 0; i--) {
+                y = sin(theta - (i * 0.05)) * amplitude;
+                fill(0, 0, 255 -(255/length * i));
+                square(x, y + height / 2, 16);
+        }
         
-        fill(0, 255, 0)
-        y  = sin(theta) * amplitude; 
-        ellipse(x, y + height/2, 16);
-        theta += 0.2;
-        //cosine
+        for (i = length; i > 0; i--) {
+            y = sin(theta - (i *0.03)) * amplitude;
+            fill(255 -(255/length * i), 0, 0);
+            square(x, y + height / 2, 16);
+    }
+    
+    
 
-        fill(0, 255, 0)
-        y  = cos(theta) * amplitude; 
-        ellipse(x, y + height/2, 16);
-        theta += 0.2;
-       
-        fill(0, 255,0 )
-        y  = cos(theta) * amplitude; 
-        ellipse(x, y + height/2, 16);
-        theta += 0.2;
-
-        fill(0, 255,0 )
-        y  = tan(theta) * amplitude; 
-        ellipse(x, y + height/2, 16);
-        theta += 0.2;
-
-        fill(0, 255,0 )
-        y  = tan(theta) * amplitude; 
-        ellipse(x, y + height/2, 16);
-        theta += 0.2;
+    
 
 
-        x = ((theta-offset)/maxAngle) * windowWidth;
+
+     theta += 0.2;
+     x = ((theta - offset) / maxAngle) * windowWidth;
     }
     offset += inc;
-    theta = offset; 
+    theta = offset;
 }
