97b7163cc63de522a9a5cfa9c1f1843d7ab5cc31
menice4 <95630289+menice4@users.noreply.github.com>
2023-01-10 14:45:11 +0000

    octopus

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,9 @@
 
 let x = 0;
 let y = 0;
-let theta = 100;
+let theta = 0;
 let inc = 0.05;
-let offset = 100; // the amount offset from begening of wave
+let offset = 0; // the amount offset from begening of wave
 let amplitude;
 let frequency;
 let maxAngle;
@@ -16,41 +16,59 @@ function setup() {
 
 }
 function draw() {
-    background(0);
+    background(0,190,255);
     noStroke();
-    let length = 50;
+    let length = 10;
     while (theta < maxAngle + offset) {
 
 
-        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/15);
+        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/7);
         for (i = length; i > 0; i--) {
-            y = tan(theta - (i * 0.1)) * amplitude;
-            fill(0, 255 -(255/length * i), 0);
-            square(x, y + width / 9, 16);
-            rotate(.1)
-        }
+            y = cos(theta - (i * 50)) * amplitude;
+            fill(255, 127-(127/length * i) ,0  );
+            circle(x, y + height / 10,90 );
+            rotate(.9)
 
+        }
+        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/3);
+        for (i = length; i > 0; i--) {
+            y = cos(theta - (i * 50)) * amplitude;
+            fill(150, 75 -(75/length * i), 0);
+            circle(x, y + height / 5, 85    );
+            rotate(.9)
+          
+
+        }
+        
+        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/3);
+        for (i = length; i > 0; i--) {
+            y = sin(theta - (i * 50)) * amplitude;
+            fill(150, 150 , 250 -(230/length * i));
+            circle(x, y + height / 5, 100
+            );
+            rotate(.2)
+
+        }
+        
+        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/3);
+        for (i = length; i > 0; i--) {
+            y = sin(theta - (i * 5)) * amplitude;
+            fill(210,215-(255/length * i) ,40 );
+            circle(x, y + height / 5, 50);
+            rotate(.2)
+
+        }
+        
+        
         
 
-        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/10.5);
-        for (i = length; i > 0; i--) {
-            y = tan(theta - (i * 0.1)) * amplitude;
-            fill(0, 255 -(255/length * i), 0);
-            circle(x, y + width / 9, 16);
-            rotate(.1)
-        }
-
-        
-
-
-    
     
 
     
 
 
 
-     theta += 0.2;
+     theta += 0.1;
      x = ((theta - offset) / maxAngle) * windowWidth;
     }
     offset += inc;
