6efc74fd136fc9fa8a51752f7121f00e62b065ae
menice4 <95630289+menice4@users.noreply.github.com>
2023-01-10 14:09:51 +0000

    circles and sqaures

--- js/sketch.js
+++ js/sketch.js
@@ -1,16 +1,16 @@
 
 let x = 0;
 let y = 0;
-let theta = 0;
+let theta = 100;
 let inc = 0.05;
-let offset = 0; // the amount offset from begening of wave
+let offset = 100; // the amount offset from begening of wave
 let amplitude;
 let frequency;
 let maxAngle;
 
 function setup() {
-    amplitude = windowHeight / 2;
-    frequency = windowWidth / 5;
+    amplitude = windowHeight / 1;
+    frequency = windowWidth / 1;
     maxAngle = (windowWidth / frequency) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
 
@@ -18,27 +18,31 @@ function setup() {
 function draw() {
     background(0);
     noStroke();
-    let length = 20;
+    let length = 50;
     while (theta < maxAngle + offset) {
 
-        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/2);
+
+        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/15);
         for (i = length; i > 0; i--) {
-            y = sin(theta - (i * 0.08)) * amplitude;
+            y = tan(theta - (i * 0.1)) * amplitude;
             fill(0, 255 -(255/length * i), 0);
-            square(x, y + height / 2, 16);
+            square(x, y + width / 9, 16);
+            rotate(.1)
         }
 
-        for (i = length; i > 0; i--) {
-                y = sin(theta - (i * 0.05)) * amplitude;
-                fill(0, 0, 255 -(255/length * i));
-                square(x, y + height / 2, 16);
-        }
         
+
+        amplitude =  (( theta-offset)/maxAngle) * (windowHeight/10.5);
         for (i = length; i > 0; i--) {
-            y = sin(theta - (i *0.03)) * amplitude;
-            fill(255 -(255/length * i), 0, 0);
-            square(x, y + height / 2, 16);
-    }
+            y = tan(theta - (i * 0.1)) * amplitude;
+            fill(0, 255 -(255/length * i), 0);
+            circle(x, y + width / 9, 16);
+            rotate(.1)
+        }
+
+        
+
+
     
     
 
